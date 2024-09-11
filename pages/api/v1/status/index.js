3fe97a38a8ca50b9b0caf94c11b0867f2f505371
Ricardo Dias <90585892+ricardo-zip@users.noreply.github.com>
2024-09-11 17:44:40 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  response.status(200).json({ status: "api online" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response.status(200).json({ chave: "api online" });
 }
 
 export default status;
